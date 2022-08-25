3d2f9338e4ecd511faab65ee9135acb03772328e
Emily Stroud <emilystroud93@gmail.com>
2022-08-24 21:54:47 -0500

    added artistData, shopData and mergedData components

---
+++ api/mergedData.js
@@ -0,0 +1,38 @@
+import { deleteArtist, getSingleArtist } from './artistData';
+import getShopArtists, { getSingleShop, deleteShop } from './shopData';
+
+// VIEW ARTIST DETAILS
+const viewArtistDetails = (artistFirebaseKey) => new Promise((resolve, reject) => {
+  getSingleArtist(artistFirebaseKey)
+    .then((artistObj) => {
+      getSingleArtist(artistObj.shopId)
+        .then((shopObj) => {
+          resolve({ shopObj, ...artistObj });
+        });
+    }).catch((error) => reject(error));
+});
+
+// VIEW SHOP DETAILS
+const viewShopDetails = (shopFirebaseKey) => new Promise((resolve, reject) => {
+  Promise.all([getSingleShop(shopFirebaseKey), getShopArtists(shopFirebaseKey)])
+    .then(([shopObj, shopArtistArray]) => {
+      resolve({ ...shopObj, artists: shopArtistArray });
+    }).catch((error) => reject(error));
+});
+
+// DELETE SHOP ARTISTS
+const deleteShopArtists = (shopId) => new Promise((resolve, reject) => {
+  getShopArtists(shopId).then((artistsArray) => {
+    const deleteArtistPromises = artistsArray.map((artist) => deleteArtist(artist.firebaseKey));
+
+    Promise.all(deleteArtistPromises).then(() => {
+      deleteShop(shopId).then(resolve);
+    });
+  }).catch((error) => reject(error));
+});
+
+export {
+  viewArtistDetails,
+  viewShopDetails,
+  deleteShopArtists,
+};
--- components/ArtistCard.js
+++ components/ArtistCard.js
@@ -1,1 +1,19 @@
-// hello
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// function ArtistCard({ artistObj, onUpdate }) {
+//   const deleteThisArtist = () => {
+//     if (window.confirm(`Delete ${artistObj.artistNane}?`)) {
+//       deleteThisArtist(artistObj.firebaseKey).then(() => onUpdate());
+//     }
+//   };
+//   return (
+
+//   );
+// }
+
+// ArtistCard.propTypes = {
+//   artistObj: PropTypes.shape({
+
+//   })
+// }
