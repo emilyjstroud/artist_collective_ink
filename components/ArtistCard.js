83d332083f17373dcfc8a54ac9bed23a1920e019
Emily Stroud <emilystroud93@gmail.com>
2022-08-26 13:53:15 -0500

    added artist and shop card

--- components/ArtistCard.js
+++ components/ArtistCard.js
@@ -1,19 +1,48 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+import { Button } from 'bootstrap';
+import Link from 'next/link';
 
-// function ArtistCard({ artistObj, onUpdate }) {
-//   const deleteThisArtist = () => {
-//     if (window.confirm(`Delete ${artistObj.artistNane}?`)) {
-//       deleteThisArtist(artistObj.firebaseKey).then(() => onUpdate());
-//     }
-//   };
-//   return (
+function ArtistCard({ artistObj, onUpdate }) {
+  const deleteThisArtist = () => {
+    if (window.confirm(`Delete ${artistObj.artistName}?`)) {
+      deleteThisArtist(artistObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={artistObj.image} alt={artistObj.artistName} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>Name: {artistObj.artistName}</Card.Title>
+        <p>Shop Name: {artistObj.shopName}</p>
+        <p>Location: {artistObj.artistLocation}</p>
+        <p>Instagram: {artistObj.igHandle}</p>
+        <Link href={`/artist/${artistObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">View Details</Button>
+        </Link>
+        <Link href={`/artist/edit/${artistObj.firebaseKey}`} passHref>
+          <Button variant="info">Edit Info</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisArtist} className="m-2">
+          Delete Artist
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
 
-//   );
-// }
+ArtistCard.propTypes = {
+  artistObj: PropTypes.shape({
+    artistName: PropTypes.string,
+    artistLocation: PropTypes.string,
+    shopName: PropTypes.string,
+    igHandle: PropTypes.string,
+    image: PropTypes.string,
+    shopId: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
 
-// ArtistCard.propTypes = {
-//   artistObj: PropTypes.shape({
-
-//   })
-// }
+export default ArtistCard;
--- components/ShopCard.js
+++ components/ShopCard.js
@@ -1,1 +1,46 @@
-// hello
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { deleteShopArtists } from '../api/mergedData';
+
+function ShopCard({ shopObj, onUpdate }) {
+  const deleteThisShop = () => {
+    if (window.confirm(`Delete ${shopObj.shopName}?`)) {
+      deleteShopArtists(shopObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={shopObj.image} alt={shopObj.shopName} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{shopObj.shopName} </Card.Title>
+        <p>Location: {shopObj.shopLocation}</p>
+        <p>Website: {shopObj.website}</p>
+        <Link href={`/shop/${shopObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">View Details</Button>
+        </Link>
+        <Link href={`/shop/edit/${shopObj.firebaseKey}`} passHref>
+          <Button variant="info">Edit Info</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisShop} className="m-2">
+          Delete Shop
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+ShopCard.propTypes = {
+  shopObj: PropTypes.shape({
+    shopName: PropTypes.string,
+    shopLocation: PropTypes.string,
+    website: PropTypes.string,
+    image: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default ShopCard;
