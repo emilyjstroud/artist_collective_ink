9472d24f50dfd2b2c5a899f04b34909fb2618431
Emily Stroud <emilystroud93@gmail.com>
2022-08-27 12:17:16 -0500

    trying to render artist cards

--- components/Search.js
+++ components/Search.js
@@ -1,1 +1,47 @@
-// hello
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import Form from 'react-bootstrap/Form';
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Button from 'react-bootstrap/Button';
+
+// function SearchBar({ filteredArtists, setFilteredArtists }) {
+//   const [searchInput, setSearchInput] = useState('');
+
+//   const handleChange = (e) => {
+//     const { value } = e.target;
+//     setSearchInput(value);
+//     const results = filteredArtists.filter((artist) => artist.name.toLowerCase().includes(value.toLowerCase()));
+//     setFilteredArtists(results);
+//   };
+
+//   const resetSearch = () => {
+//     setSearchInput('');
+//     setFilteredArtists(filteredArtists);
+//   };
+
+//   return (
+//     <div>
+//       <InputGroup className="mb-3">
+//         <Form.Control
+//           placeholder="Search Artists"
+//           aria-label="Search Artists"
+//           value={searchInput}
+//           onChange={handleChange}
+//           aria-describedby="basic-addon2"
+//         />
+//         <Button variant="danger" onClick={resetSearch}>
+//           Reset
+//         </Button>
+//       </InputGroup>
+//     </div>
+//   );
+// }
+
+// SearchBar.propTypes = {
+//   filteredArtists: PropTypes.arrayOf(PropTypes.shape({
+//     name: PropTypes.string,
+//   })).isRequired,
+//   setFilteredArtists: PropTypes.func.isRequired,
+// };
+
+// export default SearchBar;
--- pages/artist/new.js
+++ pages/artist/new.js
@@ -1,1 +1,11 @@
-// hello
+import React from 'react';
+import ArtistForm from '../../components/forms/ArtistForm';
+
+  <title>Artist Collective Ink</title>;
+
+export default function addArtist() {
+  <div>
+    <h1>Add an Artist Here</h1>
+  </div>;
+  return <ArtistForm />;
+}
