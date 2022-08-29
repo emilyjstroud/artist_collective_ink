f664c244b64df76943b7728360226eefa8c87520
Emily Stroud <emilystroud93@gmail.com>
2022-08-28 21:47:42 -0500

    added buttons

--- pages/artist.js
+++ pages/artist.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
 import { getArtists } from '../api/artistData';
 import { useAuth } from '../utils/context/authContext';
 import ArtistCard from '../components/ArtistCard';
@@ -17,15 +19,20 @@ function ArtistPage() {
   }, [user]);
 
   return (
-    <div className="d-flex flex-wrap">
-      <title>Artist Collective Ink</title>
-      <h1>Meet the Artists</h1>
-      <div className="d-flex flex-wrap flex-row">
-        {
+    <div>
+      <Link href="/artist/new" passHref>
+        <Button className="btn btn-danger">Add an Artist</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        <title>Artist Collective Ink</title>
+        <h1>Meet the Artists</h1>
+        <div className="d-flex flex-wrap flex-row">
+          {
         artists.map((artist) => (
           <ArtistCard key={artist.firebaseKey} artistObj={artist} onUpdate={getAllArtists} />
         ))
 }
+        </div>
       </div>
     </div>
   );
--- pages/shop.js
+++ pages/shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
 import { useAuth } from '../utils/context/authContext';
 import ShopCard from '../components/ShopCard';
 import { getShops } from '../api/shopData';
@@ -17,13 +19,18 @@ function ShopPage() {
   }, [user]);
 
   return (
-    <div className="d-flex flex-wrap">
-      <title>Artist Collective Ink</title>
-      <h1>Tour the Shops</h1>
-      <div className="d-flex flex-wrap flex-row">
-        {
+    <div>
+      <Link href="/shop/new" passHref>
+        <Button className="btn btn-danger">Add a Shop</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        <title>Artist Collective Ink</title>
+        <h1>Tour the Shops</h1>
+        <div className="d-flex flex-wrap flex-row">
+          {
         shops.map((shop) => <ShopCard key={shop.firebaseKey} shopObj={shop} onUpdate={getShops} />)
         }
+        </div>
       </div>
     </div>
   );
