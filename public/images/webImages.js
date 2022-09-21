30f59a8c44b0d139ccfa1b82e30d9e303cb2fa08
Emily Stroud <emilystroud93@gmail.com>
2022-09-20 20:43:24 -0500

    saved changes

--- public/images/webImages.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable jsx-a11y/alt-text */
-// import pic from './images/flash4.jpg';
-
-// function WebImage() {
-//   return <img src={pic} />;
-// }
-// export default WebImage;
